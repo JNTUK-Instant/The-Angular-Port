72e008630c0e53f73b7ef710ba07872e07186218
U-C-S <ucs3721@outlook.com>
2021-01-22 17:41:00 +0530

    v2-alpha-7.2 - CSS updates

--- src/components/inside_main.js
+++ src/components/inside_main.js
@@ -5,8 +5,8 @@ class Content extends React.Component {
   render() {
     return (
       <div className="main_content">
-        <p>This will host the content based on navibar</p>
-        <p>Version: 2-alpha</p>
+        <p>This will host the content based on Navigation-bar</p>
+        <p>Version: 2-alpha-7.2</p>
       </div>
     );
   }
@@ -16,7 +16,12 @@ class NaviBar extends React.Component {
   render() {
     return (
       <nav>
-        <p>This will be a side-Nav bar</p>
+        <div className="searchdiv">
+          <input type="text" placeholder="Search...." />
+        </div>
+        <div className="navdiv">
+          <p>Hello Visitor!</p>
+        </div>
       </nav>
     );
   }
