356469ae76118a625c12a3764b7cd0c76af395e1
U-C-S <ucs3721@outlook.com>
2021-01-23 13:24:11 +0530

    v2-alpha-7.3

--- src/components/main.js
+++ src/components/main.js
@@ -1,5 +1,31 @@
-//import react from "react";
-import { NaviBar, Content } from "./inside_main";
+import React from "react";
+import "../styles/main.css";
+
+class Content extends React.Component {
+  render() {
+    return (
+      <div className="main_content">
+        <p>This will host the content based on Navigation-bar</p>
+        <p>Version: 2-alpha-7.2</p>
+      </div>
+    );
+  }
+}
+
+class NaviBar extends React.Component {
+  render() {
+    return (
+      <nav>
+        <div className="searchdiv">
+          <input type="text" placeholder="Search...." />
+        </div>
+        <div className="navdiv">
+          <p>Hello Visitor!</p>
+        </div>
+      </nav>
+    );
+  }
+}
 
 function Main0() {
   return (
